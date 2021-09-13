18d048d3aa790ea2e3cf8f217b940b72804b8f95
ikhtiyorovich-pro <muhammadali.fayzullayev@yandex.ru>
2021-09-13 18:38:37 +0500

    next changes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,2 @@
-let x = document.querySelector(".form");
-let myquery=document.querySelector(".container-fluid");
-x.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  let name = x.querySelector("#fname").value;
-  let name = x.querySelector("#lname").value;
-  let date = x.querySelector("#tdate").value;
-  x.setAttribute("style","display: none !important");
-  var element=document.createElement("div");
-  element.classList.add("h1");
-  // element.appendChild(document.createTextNode("Hello"));
-  element.textContent=`${name} ${name} ${date}`;
-  myquery.appendChild(element); 
-  
-})
+var elFormConvert = document.querySelector(".results-form");
+var 
